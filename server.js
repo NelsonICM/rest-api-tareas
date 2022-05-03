b3b9fb6631e550526314d8d6789b3230f561870b
patylu001 <patricia.ortegon@gmail.com>
2022-05-03 18:49:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //Conexión BD
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
